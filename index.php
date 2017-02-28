809b3759915fb6a01e1d4656f0cab36ab896630b
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-02-28 12:45:39 +0200

    index.php

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: romansolomashenko
- * Date: 26.02.17
- * Time: 10:53 PM
- */
+<?php if ( have_posts() ) : ?>
+    <?php /* начинается цикл */ ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+        <?php
+            //Предназначена для поиска и подключения разных частей темы. Похожа на PHP функцию include(),
+            // только тут не нужно указывать путь до темы.
+            get_template_part( 'template-parts/content', get_post_format() );
+        ?>
+<?php endwhile; ?>
+<?php else : ?>
+    <?php get_template_part( 'template-parts/content', 'none' ); ?>
+<?php endif; ?>
---
+++ template-parts/content.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romansolomashenko
+ * Date: 28.02.17
+ * Time: 12:39 PM
+ */
