6ec516dca3c09924e0446f8150653152a2c2907b
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-02-28 13:38:56 +0200

    sidebar.php

--- index.php
+++ index.php
@@ -7,20 +7,35 @@
   */
 get_header();
 ?>
+    <main id="content" role="main">
+        <div class="section">
+            <div class="container">
+                <div class="row">
+                    <div class="three-quarters-block">
+                        <?php if ( have_posts() ) : ?>
+                            <?php /* начинается цикл */ ?>
+                            <?php while ( have_posts() ) : the_post(); ?>
+                                <?php
+                                    //Предназначена для поиска и подключения разных частей темы. Похожа на PHP функцию include(),
+                                    // только тут не нужно указывать путь до темы.
+                                    get_template_part( 'template-parts/content', get_post_format() );
+                                ?>
+                        <?php endwhile; ?>
+                        <?php else : ?>
+                            <?php get_template_part( 'template-parts/content', 'none' ); ?>
+                        <?php endif; ?>
+                    </div>
 
-<?php if ( have_posts() ) : ?>
-    <?php /* начинается цикл */ ?>
-    <?php while ( have_posts() ) : the_post(); ?>
-        <?php
-            //Предназначена для поиска и подключения разных частей темы. Похожа на PHP функцию include(),
-            // только тут не нужно указывать путь до темы.
-            get_template_part( 'template-parts/content', get_post_format() );
-        ?>
-<?php endwhile; ?>
-<?php else : ?>
-    <?php get_template_part( 'template-parts/content', 'none' ); ?>
-<?php endif; ?>
+                    <!-- sidebar -->
 
+                    <!-- end sidebar -->
+
+                </div> <!-- .row -->
+            </div> <!-- .container -->
+        </div> <!-- .section -->
+
+
+    </main> <!-- #content -->
 
 
 <?php
